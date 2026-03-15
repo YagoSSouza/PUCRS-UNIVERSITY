80122a71e4e450593d9cbe4d2cd3d1c80353e0cc
Yago Souza <124635178+YagoSSouza@users.noreply.github.com>
2026-03-15 05:31:26 -0300

    Update average-grade.py

--- PUCRS-University/python-university-exercises/conditionals/average-grade.py
+++ PUCRS-University/python-university-exercises/conditionals/average-grade.py
@@ -1,11 +1,17 @@
+# Solicita três notas ao usuário
 n1 = float(input("Nota 1:"))
 n2 = float(input("Nota 2:"))
 n3 = float(input("Nota 3:"))
 
+# Verifica qual das três notas é a maior
 if n1>=n2 and n1>=n3:
+  # Calcula a média ponderada dando peso maior para a maior nota
   media = (n1*5 + n2*2.5 + n3*2.5 / 10 )
 elif n2>=n1 and n2>=n3:
+  # Caso a segunda nota seja a maior
   media = (n2*5 + n1*2.5 + n3*2.5 / 10)
 elif n3>=n1 and n3>=n2:
+  # Caso a terceira nota seja a maior
   media = (n3*5 + n1*2.5 + n2*2.5 / 10)
+# Exibe o resultado da média
 print("A nota média é: ", media)
