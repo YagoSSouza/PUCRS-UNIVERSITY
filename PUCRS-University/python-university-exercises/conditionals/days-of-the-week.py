da7bbe65a8bd5b4a1a7427d3e45fac3b348d0286
Yago Souza <124635178+YagoSSouza@users.noreply.github.com>
2026-03-15 05:38:14 -0300

    Update days-of-the-week.py

--- PUCRS-University/python-university-exercises/conditionals/days-of-the-week.py
+++ PUCRS-University/python-university-exercises/conditionals/days-of-the-week.py
@@ -1,17 +1,22 @@
-dia = int(input(Digite um número (1-7) ))
-if dia ==1 
-  print(Domingo)
-elif dia ==2 
-  print(Segunda)
-elif dia ==3 
-  print(Terça)
-elif dia ==4 
-  print(Quarta)
-elif dia ==5 
-  print(Quinta)
-elif dia ==6 
-  print(Sexta)
-elif dia ==7 
-  print(Sábado)
-else
-  print(Erro. Insira um valor no intervalo de 1-7)
+# Solicita ao usuário um número entre 1 e 7
+dia = int(input("Digite um número (1-7): "))
+
+# Verifica qual dia da semana corresponde ao número digitado
+if dia == 1:
+  print("Domingo")
+elif dia == 2:
+  print("Segunda")
+elif dia == 3:
+  print("Terça")
+elif dia == 4:
+  print("Quarta")
+elif dia == 5:
+  print("Quinta")
+elif dia == 6:
+  print("Sexta")
+elif dia == 7:
+  print("Sábado")
+  
+# Caso o número não esteja entre 1 e 7
+else:
+  print("Erro. Insira um valor no intervalo de 1-7")
